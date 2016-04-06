31eeba16f7a8cc37aaeac78f1c2a56a629524c0e
yuvrajtiwari <yuvraj.tiwari@ymail.com>
2016-04-06 17:16:00 -0500

    Update Solution.java

--- Intersection of Two Linked Lists/Solution.java
+++ Intersection of Two Linked Lists/Solution.java
@@ -14,36 +14,82 @@ public class Solution {
      
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
         
-
-        Map<ListNode,Integer> map=new TreeMap<ListNode,Integer>( new Comparator<ListNode>() {
-                
-                @Override
-                public int compare(ListNode e1,
-                        ListNode e2) {
-                    return e1.val-(e2.val);
-                }
-            }); 
+       
+       
+         ListNode temp = headA ;
+         ListNode returnNode = null ;
+         int list1size = 0 ;
+         int list2size = 0 ;
+         while(temp != null){
+             list1size ++ ;
+          
+            temp = temp.next ;
+         
+             
+         }
+         
+         temp = headB ;
+         
+         while( temp != null){
+             
+             list2size ++ ;
+             
+             temp = temp.next ;
+             
+         }
         
-        ListNode current=headA;
+
+        
+        if(list1size > list2size){
+            
+            int diff = list1size - list2size ;
+            
+            while(diff>0){
+                
+                headA = headA.next ;
+                diff--;
+            }
        
-        while(current!=null)
-        {
-           
-            map.put(current,1);
-            current=current.next;
         }
-        ListNode current1=headB;
-       
-        while(current1!=null)
-        {
-            if(map.containsKey(current1))
-                return current1;
-            else    
-               
-            current1=current1.next;
+        else{
+            
+           int diff = list2size - list1size;
+            
+            while(diff>0){
+                
+                headB = headB.next ;
+                diff-- ;
+            }
+            
+      
+            
+            
+        }
+        
+
+        returnNode = findIntersection(headB,headA);
+        return returnNode ;
+        
+    }
+    
+    ListNode findIntersection(ListNode longerListHead, ListNode shorterListHead){
+        
+        while(longerListHead != null && shorterListHead != null){
+            
+            if(longerListHead.val == shorterListHead.val){
+                
+                return longerListHead ;
+            }
+            
+                longerListHead = longerListHead.next ;
+                shorterListHead = shorterListHead.next ;
+            
+            
         }
         return null ;
-}
-
+        
+    }
+    
+  
     
 }
